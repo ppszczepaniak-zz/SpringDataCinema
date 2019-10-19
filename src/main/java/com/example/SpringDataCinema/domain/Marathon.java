f8bfce92e4a0244a3834924fc0560de6caf45bd5
ppszczepaniak <p.p.szczepaniak@gmail.com>
2019-10-19 13:48:14 +0200

    add Marathon class code

--- src/main/java/com/example/SpringDataCinema/domain/Marathon.java
+++ src/main/java/com/example/SpringDataCinema/domain/Marathon.java
@@ -1,4 +1,76 @@
 package com.example.SpringDataCinema.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.StringJoiner;
+
+@Entity
 public class Marathon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String name;
+    private LocalDateTime startTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public Marathon(Long id, String name, LocalDateTime startTime) {
+        this.id = id;
+        this.name = name;
+        this.startTime = startTime;
+    }
+
+    public Marathon() {
+    }
+
+    /* these are best implementations of equals() & hashCode() for entities objects
+    why? read this: https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
+    */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Marathon)) return false;
+        Marathon other = (Marathon) o;
+        return id != null && id.equals(other.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return 31;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Marathon.class.getSimpleName() + "[", "]")
+                .add("id=" + id)
+                .add("name='" + name + "'")
+                .add("startTime=" + startTime)
+                .toString();
+    }
 }
