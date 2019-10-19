8480972e77805caa5c5fa3d908163841100a5ea4
ppszczepaniak <p.p.szczepaniak@gmail.com>
2019-10-19 17:00:50 +0200

    add MovieService code

--- src/main/java/com/example/SpringDataCinema/service/MovieService.java
+++ src/main/java/com/example/SpringDataCinema/service/MovieService.java
@@ -1,9 +1,25 @@
 package com.example.SpringDataCinema.service;
 
 import com.example.SpringDataCinema.domain.EMovieCategory;
+import com.example.SpringDataCinema.domain.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
 
 public interface MovieService {
 
     Long createMovie(String title, EMovieCategory category, Integer length, String description, Integer requiredAge, String posterFilePath);
 
+    Optional<Movie> getMovie(Long movieId);
+
+    Page<Movie> getMoviesInCategory(EMovieCategory category, Pageable pageable);
+
+    Page<Movie> getMoviesByPartOfTitle(String partOfTitle, Pageable pageable);
+
+    Page<Movie> getAllMovies(Pageable pageable);
+
+    void updateMovie(Long movieId, String title, EMovieCategory category, Integer length, String description, Integer requiredAge, String posterFilePath);
+
+    void removeMovie(Long movieId);
 }
