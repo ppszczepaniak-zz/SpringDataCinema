5431c183a79a777846c6aaa3129f1a39fe122ab5
ppszczepaniak <p.p.szczepaniak@gmail.com>
2019-10-19 13:45:35 +0200

    add Ticket & Session class code

--- src/main/java/com/example/SpringDataCinema/domain/Session.java
+++ src/main/java/com/example/SpringDataCinema/domain/Session.java
@@ -1,4 +1,65 @@
 package com.example.SpringDataCinema.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.StringJoiner;
+
+@Entity
 public class Session {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private LocalDateTime startTime;
+
+    public Session(Long id, LocalDateTime startTime) {
+        this.id = id;
+        this.startTime = startTime;
+    }
+
+    public Session() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    /* these are best implementations of equals() & hashCode() for entities objects
+why? read this: https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
+*/
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Session)) return false;
+        Session other = (Session) o;
+        return id != null && id.equals(other.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return 31;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Session.class.getSimpleName() + " [", "]")
+                .add("id=" + id)
+                .add("startTime=" + startTime)
+                .toString();
+    }
 }
--- src/main/java/com/example/SpringDataCinema/domain/Ticket.java
+++ src/main/java/com/example/SpringDataCinema/domain/Ticket.java
@@ -1,4 +1,76 @@
 package com.example.SpringDataCinema.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.util.StringJoiner;
+
+@Entity
 public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String seat;
+    private BigDecimal price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSeat() {
+        return seat;
+    }
+
+    public void setSeat(String seat) {
+        this.seat = seat;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Ticket(Long id, String seat, BigDecimal price) {
+        this.id = id;
+        this.seat = seat;
+        this.price = price;
+    }
+
+    public Ticket() {
+    }
+
+    /* these are best implementations of equals() & hashCode() for entities objects
+why? read this: https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
+*/
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Ticket)) return false;
+        Ticket other = (Ticket) o;
+        return id != null && id.equals(other.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return 31;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Ticket.class.getSimpleName() + " [", "]")
+                .add("id=" + id)
+                .add("seat='" + seat + "'")
+                .add("price=" + price)
+                .toString();
+    }
 }
