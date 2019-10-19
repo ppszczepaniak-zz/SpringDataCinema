dff577b7dc25770b1cc0d2746b8ab5071b0c9999
ppszczepaniak <p.p.szczepaniak@gmail.com>
2019-10-19 16:32:30 +0200

    add IterableUtils class code

--- src/main/java/com/example/SpringDataCinema/service/MovieService.java
+++ src/main/java/com/example/SpringDataCinema/service/MovieService.java
@@ -1,4 +1,9 @@
 package com.example.SpringDataCinema.service;
 
+import com.example.SpringDataCinema.domain.EMovieCategory;
+
 public interface MovieService {
+
+    Long createMovie(String title, EMovieCategory category, Integer length, String description, Integer requiredAge, String posterFilePath);
+
 }
--- src/main/java/com/example/SpringDataCinema/utils/IterableUtils.java
+++ src/main/java/com/example/SpringDataCinema/utils/IterableUtils.java
@@ -1,4 +1,13 @@
 package com.example.SpringDataCinema.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class IterableUtils {
+
+    public static <T> List<T> iterableToList(Iterable<T> iterable) {
+        List<T> resultList = new ArrayList<>();
+        iterable.forEach(t -> resultList.add(t));
+        return resultList;
+    }
 }
