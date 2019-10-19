b7dd8025179873e1c32afd2c2a1eabf667d95432
ppszczepaniak <p.p.szczepaniak@gmail.com>
2019-10-19 16:26:34 +0200

    add exception class code

--- src/main/java/com/example/SpringDataCinema/exception/BusinessException.java
+++ src/main/java/com/example/SpringDataCinema/exception/BusinessException.java
@@ -1,4 +1,22 @@
 package com.example.SpringDataCinema.exception;
 
-public class BusinessException {
+public class BusinessException extends RuntimeException{
+    public BusinessException() {
+    }
+
+    public BusinessException(String message) {
+        super(message);
+    }
+
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BusinessException(Throwable cause) {
+        super(cause);
+    }
+
+    public BusinessException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
--- src/main/java/com/example/SpringDataCinema/exception/EntityDoesNotExistException.java
+++ src/main/java/com/example/SpringDataCinema/exception/EntityDoesNotExistException.java
@@ -1,4 +1,22 @@
 package com.example.SpringDataCinema.exception;
 
-public class EntityDoesNotExistException {
+public class EntityDoesNotExistException extends RuntimeException {
+    public EntityDoesNotExistException() {
+    }
+
+    public EntityDoesNotExistException(String message) {
+        super(message);
+    }
+
+    public EntityDoesNotExistException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public EntityDoesNotExistException(Throwable cause) {
+        super(cause);
+    }
+
+    public EntityDoesNotExistException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
