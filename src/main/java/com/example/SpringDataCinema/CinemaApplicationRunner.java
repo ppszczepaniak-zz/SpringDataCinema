d2028b91683f4ec795beefa3f296373ca3efe383
ppszczepaniak <p.p.szczepaniak@gmail.com>
2019-10-20 12:35:19 +0200

    add CinemaApplicationRunner

--- src/main/java/com/example/SpringDataCinema/CinemaApplicationRunner.java
+++ src/main/java/com/example/SpringDataCinema/CinemaApplicationRunner.java
@@ -1,8 +1,6 @@
 package com.example.SpringDataCinema;
 
-import com.example.SpringDataCinema.domain.EMovieCategory;
-import com.example.SpringDataCinema.domain.Movie;
-import com.example.SpringDataCinema.domain.Session;
+import com.example.SpringDataCinema.domain.*;
 import com.example.SpringDataCinema.service.MarathonService;
 import com.example.SpringDataCinema.service.MovieService;
 import com.example.SpringDataCinema.service.SessionService;
@@ -15,8 +13,10 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -38,9 +38,10 @@ public class CinemaApplicationRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
         movieServiceInvocations();
         sessionServiceInvocations();
+        ticketServiceInvocations();
+        marathonServiceInvocations();
     }
 
     private void movieServiceInvocations() {
@@ -77,7 +78,6 @@ public class CinemaApplicationRunner implements CommandLineRunner {
         LOG.info("7. sessionsIn02052019. AmountOfSessions={}", sessionsIn02052019.size());
         sessionsIn02052019.forEach(session -> LOG.info(" {}", session));
 
-
         Session sessionWithId15 = sessionService.getSession(15L).get();
         LOG.info("8. sessionWithId15 - {}, MovieTitle='{}', Room='{}'", sessionWithId15, sessionWithId15.getMovie().getTitle(), sessionWithId15.getRoom().getName());
         // sessionWithId15.getTickets().forEach(ticket -> LOG.info(" {}", ticket));
@@ -95,9 +95,37 @@ public class CinemaApplicationRunner implements CommandLineRunner {
         Long newSessionId = sessionService.createSession(11L, 4L, LocalDateTime.of(2019, 5, 3, 10, 30));
         Session newSession = sessionService.getSession(newSessionId).get();
         LOG.info("10. newSession - {}", newSession);
-        
+
         sessionService.removeSession(16L);
     }
 
+    private void ticketServiceInvocations() {
+        List<Ticket> ticketsOnSessionWithId15 = ticketService.getAllTicketsForSession(15L);
+        LOG.info("11. ticketsOnSessionWithId15. AmountOfTickets={}", ticketsOnSessionWithId15.size());
+        ticketsOnSessionWithId15.forEach(ticket -> LOG.info(" {}", ticket));
+
+        Long newTicketId = ticketService.newTicket(15L, "r2m5", new BigDecimal("22.50"));
+        LOG.info("12. newTicketId={}", newTicketId);
+
+        ticketService.cancelTicket(18L);
+
+        List<Ticket> ticketsOnSessionWithId15AfterUpdates = ticketService.getAllTicketsForSession(15L);
+        LOG.info("13. ticketsOnSessionWithId15AfterUpdates. AmountOfTickets={}", ticketsOnSessionWithId15AfterUpdates.size());
+        ticketsOnSessionWithId15AfterUpdates.forEach(ticket -> LOG.info(" {}", ticket));
+
+    }
+
+    private void marathonServiceInvocations() {
+        Long newMarathonId = marathonService.createMarathon("Maraton horrorów, 30.10.2018", LocalDateTime.of(2018, 10, 30, 20, 0), Arrays.asList(6L, 8L, 9L));
+        Marathon newMarathon = marathonService.getMarathon(newMarathonId).get();
+        LOG.info("14. newMarathon - {}", newMarathon);
+        newMarathon.getMovies().forEach(movie -> LOG.info(" {}", movie));
+
+        List<Marathon> marathons = marathonService.getAllMarathons();
+        LOG.info("15. marathons. AmountOfMarathons={}", marathons.size());
+        marathons.forEach(marathon -> LOG.info(" {}", marathon));
+
+        marathonService.removeMarathon(newMarathonId);
+    }
 
 }
