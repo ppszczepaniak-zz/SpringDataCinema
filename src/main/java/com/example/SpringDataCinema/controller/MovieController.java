b0683d5a2050cf01c4ca65f360f05568f8ec6c92
ppszczepaniak <p.p.szczepaniak@gmail.com>
2019-10-20 16:18:10 +0200

    add MovieController (REST)

--- src/main/java/com/example/SpringDataCinema/controller/MovieController.java
+++ src/main/java/com/example/SpringDataCinema/controller/MovieController.java
@@ -1,14 +1,15 @@
 package com.example.SpringDataCinema.controller;
 
+import com.example.SpringDataCinema.domain.EMovieCategory;
 import com.example.SpringDataCinema.domain.Movie;
 import com.example.SpringDataCinema.service.MovieService;
+import com.example.SpringDataCinema.utils.IterableUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/movie")
@@ -22,36 +23,45 @@ public class MovieController {
 
     @GetMapping("/getAll") //http://localhost:8080/movie/getAll or use SWAGGER :)
     @ResponseStatus(HttpStatus.OK)
-    public Page<Movie> getBooks() {
+    public Page<Movie> getMovies() {
         return movieService.getAllMovies(Pageable.unpaged());
     }
 
-//    @PostMapping("/add") //http://localhost:8080/movie/add
-//    @ResponseStatus(HttpStatus.CREATED) //201
-//    public Movie addMovie(@RequestBody Movie movie) {
-//        return movieService.createMovie()
+    @PostMapping("/add") //http://localhost:8080/movie/add
+    @ResponseStatus(HttpStatus.CREATED) //201
+    public Long addMovie(@RequestBody Movie movie, @RequestHeader String posterFilePath) {
+        return movieService.createMovie(movie.getTitle(), movie.getCategory(), movie.getLength(), movie.getDescription(), movie.getRequiredAge(), posterFilePath);
+    }
 
-//    Long createMovie(String title, EMovieCategory category, Integer length, String description, Integer requiredAge, String posterFilePath);
-//
-//    Optional<Movie> getMovie(Long movieId);
-//
-//    Page<Movie> getMoviesInCategory(EMovieCategory category, Pageable pageable);
-//
-//    Page<Movie> getMoviesByPartOfTitle(String partOfTitle, Pageable pageable);
-//
-//    Page<Movie> getAllMovies(Pageable pageable);
-//
-//    void updateMovie(Long movieId, String title, EMovieCategory category, Integer length, String description, Integer requiredAge, String posterFilePath);
-//
-//    void removeMovie(Long movieId);
+    @GetMapping("/get") //http://localhost:8080/movie/get
+    @ResponseStatus(HttpStatus.OK)
+    public Movie getMovie(@RequestParam("movieId") long movieId) {  //could be @Param (different Annotation from data.repository.query
+        return movieService.getMovie(movieId).get();
+    }
 
-//
-//
-//
-//        @GetMapping("/get") ////http://localhost:8080/book/get
-//        public Book getBook(@RequestParam("bookId") long bookId) {  //could be @Param (different Annotation from data.repository.query
-//            return bookService.get(bookId);
-//        }
+    @DeleteMapping("/delete") //http://localhost:8080/movie/delete
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteMovie(@RequestParam("movieId") long movieId) {
+        movieService.removeMovie(movieId);
+    }
+
+    @GetMapping("/getInCategory")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Movie> getMovieByCategory(@RequestParam("category") EMovieCategory category) {
+        return IterableUtils.iterableToList(movieService.getMoviesInCategory(category, Pageable.unpaged()));
+    }
+
+    @GetMapping("/getByPartOfTitle")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Movie> getMovieByPartOfTitle(@RequestParam("partOfTitle") String partOfTitle) {
+        return IterableUtils.iterableToList(movieService.getMoviesByPartOfTitle(partOfTitle, Pageable.unpaged()));
+    }
+
+    @PostMapping("/update")
+    @ResponseStatus(HttpStatus.OK)
+    public void updateMovie(@RequestBody Movie movie, @RequestHeader String posterFilePath) {
+        movieService.updateMovie(movie.getId(), movie.getTitle(), movie.getCategory(), movie.getLength(), movie.getDescription(), movie.getRequiredAge(), posterFilePath);
+    }
 
 }
 
